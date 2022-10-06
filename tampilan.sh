a1903d73cfc8cc981004b54b6f946290099d440a
ProjectCoFi <85388584+ProjectCoFi@users.noreply.github.com>
2022-10-06 17:38:34 +0700

    Add files via upload

---
+++ tampilan.sh
@@ -0,0 +1,24 @@
+sleep 1
+tam1="================================="                                                          
+tam2="{  Selamat datang sobat h3ngker!  }"
+
+# login
+toilet -f big -F gay LOGIN
+echo "Masukan Password" | lolcat
+read pass
+
+# data                                                                                                                if [ $pass = AntzF517 ]
+then
+		figlet H3NGK3R | lolcat
+		echo $tam1 | lolcat # tampilan 1
+		echo $tam2 | lolcat # tampilan 2
+		echo $tam1 | lolcat # tampilan 1
+		echo "# Anonymous" | lolcat
+		echo "# Cyber Unity" | lolcat
+		echo "# Termux" | lolcat
+		echo $tam1 | lolcat # tampilan 1
+else
+		echo "Password Salah" | lolcat
+		echo $tam1 | lolcat # tampilan 1
+		sh tampilan.sh
+fi
